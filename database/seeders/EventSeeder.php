ef13c6440b124ec60fd3ef94e2194ac1d31eee0e
DarioGuastella <jimmyguastella@gmail.com>
2024-02-05 17:51:58 +0100

    popola eventSeeder

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,34 @@ class EventSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $events = [
+            [
+                'name' => 'Concerto 1',
+                'date' => '2024-01-22',
+                'available_tickets' => 150
+            ],
+            [
+                'name' => 'Concerto 2',
+                'date' => '2024-02-12',
+                'available_tickets' => 300
+            ],
+            [
+                'name' => 'Concerto 3',
+                'date' => '2024-01-15',
+                'available_tickets' => 300
+            ],
+            [
+                'name' => 'Concerto 4',
+                'date' => '2024-03-15',
+                'available_tickets' => 150
+            ]
+        ];
+
+        foreach ($events as $event) {
+
+            $newEvent = new Event();
+            $newEvent->fill($event);
+            $newEvent->save();
+        }
     }
 }
